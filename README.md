5c1bef34489324052b7c95f94930e8d2df585cc5
Rex Garland <rex.garland@gmail.com>
2021-11-09 17:40:10 -0600

    changed to model

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
 # beep-designer
 
-Because you deserve the perfect beep
+Because you deserve the perfect beep.
+
+Design a beep based on this model:
+[model.gif](model.jpg)
+
+A level of -6 corresponds to an sustain amplitude that is about half the peak.
 
 ## Run from the shell
 
+Replace any variable in the model above on the command line.
+
 ```shell
-python3 create-beep.py --frequency 880 --duration 0.1 --ramp 0.01 --output beep.wav
+python3 create-beep.py --frequency 440 --peak 0.5 --sustain 0.1 --output beep.wav
 ```
+
+All parameters have defaults, so this also works:
+
+```shell
+python3 create-beep.py
+```
+
+See the end of `create-beep.py` for defaults.
--- create-beep.py
+++ create-beep.py
@@ -4,37 +4,63 @@ import struct
 
 SAMPLE_RATE = 44100
 
-def create_beep_data(frequency, amplitude, duration, ramp):
+class Model:
+    def __init__(self, args):
+        self.attack = getattr(args, 'attack')
+        self.peak = getattr(args, 'peak')
+        self.decay = getattr(args, 'decay')
+        self.level = getattr(args, 'level')
+        self.sustain = getattr(args, 'sustain')
+        self.release = getattr(args, 'release')
+
+        assert self.peak >= 0 and self.peak <= 1, "Peak must be between 0 and 1"
+        assert self.level <= 0, "Level must be in dB, less than 0"
+
+        self.sustain_amplitude = self.peak * 10**(float(self.level) / 20)
+
+        times = [
+            self.attack,
+            self.attack+self.decay,
+            self.attack+self.decay+self.sustain,
+            self.attack+self.decay+self.sustain+self.release
+        ]
+        self.divisions = [int(time*SAMPLE_RATE) for time in times]
+
+        self.attack_length = self.divisions[0]
+        self.decay_length = self.divisions[1] - self.divisions[0]
+        self.release_length = self.divisions[3] - self.divisions[2]
+        self.num_samples = self.divisions[-1]
+
+    def amplitude_at(self, index):
+        if index < self.divisions[0]: # in attack
+            return self.peak * (float(index) / self.attack_length)
+        elif index < self.divisions[1]: # in decay
+            drop = self.peak - self.sustain_amplitude
+            since_peak = index-self.divisions[0]
+            return self.peak - drop * (float(since_peak) / self.decay_length)
+        elif index < self.divisions[2]: # in sustain
+            return self.sustain_amplitude
+        else: # in release
+            to_end = max(self.divisions[3] - index, 0)
+            return self.sustain_amplitude * (to_end / self.release_length)
+
+
+def create_beep_data(frequency, envelope_model):
     """
     Creates an array of floating point number represent the audio data of the beep.
     """
 
     assert frequency > 0 and frequency < SAMPLE_RATE//2, f"Frequency must be above 0 and below {SAMPLE_RATE//2}"
-    assert amplitude >= 0 and amplitude <= 1, "Amplitude must be between 0 and 1, inclusive"
-    assert ramp*2 < duration, "The ramp is too long. It is applied to both sides and it counted as part of |duration|."
 
     data = []
     
-    num_samples = int(duration * SAMPLE_RATE)
     omega = 2 * math.pi * frequency / SAMPLE_RATE
 
-    ramp_samples = int(ramp * SAMPLE_RATE)
-    ramp_omega = math.pi * (1.0 / ramp) / SAMPLE_RATE
-
-    for i in range(ramp_samples):
-        ramp_amplitude = amplitude * (0.5 - 0.5 * math.cos( ramp_omega * i ))
-        data.append(ramp_amplitude * math.sin( omega * i ))
-
-    for i in range(ramp_samples, num_samples - ramp_samples):
-        data.append(amplitude * math.sin( omega * i ))
-
-    for i in range(num_samples - ramp_samples, num_samples):
-        j = i - (num_samples - ramp_samples)
-        ramp_amplitude = amplitude * (0.5 + 0.5 * math.cos( ramp_omega * j ))
-        data.append(ramp_amplitude * math.sin( omega * i ))
+    for i in range(envelope_model.num_samples):
+        amplitude = envelope_model.amplitude_at(i)
+        data.append(amplitude * math.sin(omega * i))
 
     return data
-
 
 def save(data, wavfile):
     with wave.open(wavfile, "w") as f:
@@ -50,11 +76,15 @@ if __name__=="__main__":
     import argparse
     ap = argparse.ArgumentParser(description='Create a beep sound')
     ap.add_argument('--frequency', default=440.0, type=float, help=f'frequency in Hz, less than {SAMPLE_RATE//2}')
-    ap.add_argument('--amplitude', default=1.0, type=float, help='a float, in the range [0,1]')
-    ap.add_argument('--duration', default=0.1, type=float, help='the total duration of the sound, in seconds')
-    ap.add_argument('--ramp', default=0.01, type=float, help='the duration of the quarter-cosine ramp, in seconds, applied to boths sides of the sound (c.f. "tukey window")')
+    ap.add_argument('--attack', default=0.002, type=float, help=f'attack duration, in seconds (float)')
+    ap.add_argument('--peak', default=1.0, type=float, help='peak amplitude, in range [0,1] (float)')
+    ap.add_argument('--decay', default=0.004, type=float, help='decay duration, in seconds (float)')
+    ap.add_argument('--sustain', default=0.04, type=float, help='sustain duration, in seconds (float)')
+    ap.add_argument('--level', default=-6, type=float, help='sustain level, in dB (negative float)')
+    ap.add_argument('--release', default=0.004, type=float, help='release duration, in seconds (float)')
     ap.add_argument('--output', default='beep.wav', help='output wav filename')
     args = ap.parse_args()
 
-    data = create_beep_data(args.frequency, args.amplitude, args.duration, args.ramp)
+    model = Model(args)
+    data = create_beep_data(args.frequency, model)
     save(data, args.output)
